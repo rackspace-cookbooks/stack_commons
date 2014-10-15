f4dc66a6dbace1756c5783b5f4908af384b8fff8
Julien Berard <jujugrrr@gmail.com>
2014-10-15 17:02:41 +0100

    Test a different mongo service name for Redhat based platform

--- test/integration/runlist_mongodb_standalone/serverspec/default_spec.rb
+++ test/integration/runlist_mongodb_standalone/serverspec/default_spec.rb
@@ -2,10 +2,17 @@
 
 require_relative 'spec_helper'
 
-describe service('mongodb') do
+# apache
+if os[:family] == 'redhat'
+  mongo_service = 'mongod'
+else
+  mongo_service = 'mongodb'
+end
+
+describe service(mongo_service) do
   it { should be_enabled }
 end
 
-describe service('mongodb') do
+describe service(mongo_service) do
   it { should be_running }
 end
