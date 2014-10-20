ee28ba2b7e496f5380c8587f59e5bcbd836d797d
Martin Smith <martin@mbs3.org>
2014-10-20 09:19:06 -0500

    Freshen tests with real content

--- test/unit/spec/redis_spec.rb
+++ test/unit/spec/redis_spec.rb
@@ -11,21 +11,57 @@ describe 'stack_commons::redis_base' do
       # Context for each platform
       context "on #{platform.capitalize} #{version}" do
         let(:chef_run) do
-          ChefSpec::SoloRunner.new(platform: platform, version: version, log_level: LOG_LEVEL) do |node|
+          ChefSpec::ServerRunner.new(platform: platform, version: version, log_level: LOG_LEVEL) do |node, server|
             node_resources(node)
-          end.converge('stack_commons::redis_master', 'stack_commons::redis_slave', 'stack_commons::redis_sentinel')
+          end.converge('stack_commons::redis_single')
         end
 
         it 'installs redis' do
+          expect(chef_run).to include_recipe('redis-multi')
+          expect(chef_run).to include_recipe('redis-multi::enable')
+        end
+      end
+
+      context 'redis-master' do
+        let(:chef_run) do
+          ChefSpec::ServerRunner.new(platform: platform, version: version, log_level: LOG_LEVEL) do |node, server|
+            node_resources(node)
+          end.converge('stack_commons::redis_master')
         end
 
         it 'configures a redis master' do
+          expect(chef_run).to include_recipe('redis-multi')
+          expect(chef_run).to include_recipe('redis-multi::enable')
+        end
+
+      end
+
+      context 'redis-slave' do
+        let(:chef_run) do
+          ChefSpec::ServerRunner.new(platform: platform, version: version, log_level: LOG_LEVEL) do |node, server|
+            node_resources(node)
+            node.set['redis-multi']['redis_master'] = '127.0.0.1'
+          end.converge('stack_commons::redis_slave')
         end
 
         it 'configures a redis slave' do
+          expect(chef_run).to include_recipe('redis-multi')
+          expect(chef_run).to include_recipe('redis-multi::enable')
+        end
+
+      end
+
+      context 'redis-sentinel' do
+        let(:chef_run) do
+          ChefSpec::ServerRunner.new(platform: platform, version: version, log_level: LOG_LEVEL) do |node, server|
+            node_resources(node)
+            node.set['redis-multi']['redis_master'] = '127.0.0.1'
+          end.converge('stack_commons::redis_sentinel')
         end
 
         it 'configures a redis sentinel' do
+          expect(chef_run).to include_recipe('redis-multi::sentinel')
+          expect(chef_run).to include_recipe('redis-multi::sentinel_enable')
         end
 
       end
@@ -33,7 +69,7 @@ describe 'stack_commons::redis_base' do
       # context for elkstack logging enabled
       context 'with ELKstack logging enabled' do
         let(:chef_run) do
-          ChefSpec::SoloRunner.new(platform: platform, version: version, log_level: LOG_LEVEL) do |node|
+          ChefSpec::ServerRunner.new(platform: platform, version: version, log_level: LOG_LEVEL) do |node, server|
             node_resources(node)
             node.set['logstash_commons']['restart_service'] = false # need logstash cookbook to do this
             node.set['platformstack']['elkstack_logging']['enabled'] = true
