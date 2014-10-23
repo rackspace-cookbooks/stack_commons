cf1a2f2844eeba10da148fce40e337e45ed237de
Julien Berard <jujugrrr@gmail.com>
2014-10-23 11:55:12 +0100

    Deleted old unused test files

--- test/fixtures/cookbooks/wrapper/recipes/memcached.rb
+++
@@ -1,8 +0,0 @@
-#
-# Cookbook Name:: wrapper
-# Recipe:: memcached
-#
-# Copyright 2014, Rackspace
-#
-#
-include_recipe  'stack_commons::memcached'
--- test/fixtures/cookbooks/wrapper/recipes/mongodb_standalone.rb
+++
@@ -1,8 +0,0 @@
-#
-# Cookbook Name:: wrapper
-# Recipe:: default
-#
-# Copyright 2014, Rackspace
-#
-#
-include_recipe  'stack_commons::mongodb_standalone'
