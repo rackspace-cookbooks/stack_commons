608a4dd1b41623ca23285da9f4c0c3e7f82f8b22
Martin Smith <martin@mbs3.org>
2014-10-23 10:46:55 -0500

    version dump after merge with upstream, since the upstream version was the same bump as mine

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email 'rackspace-cookbooks@rackspace.com'
 license 'Apache 2.0'
 description 'provides common supporting features for stacks'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.0.11'
+version '0.0.12'
 
 depends 'apache2'
 depends 'apt'
