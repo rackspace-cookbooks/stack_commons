f196a2eaaca78a944cf086b3c4d3a1e6aedb7b77
Martin Smith <martin@mbs3.org>
2014-12-01 11:21:11 -0600

    Rubocop likes spaces

--- test/integration/default/serverspec/newrelic_spec.rb
+++ test/integration/default/serverspec/newrelic_spec.rb
@@ -2,27 +2,26 @@ require_relative 'spec_helper'
 
 describe 'configures newrelic meetme for nginx' do
   context file('/etc/newrelic/newrelic-plugin-agent.cfg') do
-    its(:content) { should match(/nginx/)}
+    its(:content) { should match(/nginx/) }
   end
 end
 
 describe 'configures newrelic meetme for uwsgi' do
   context file('/etc/newrelic/newrelic-plugin-agent.cfg') do
-    its(:content) { should match(/uwsgi/)}
+    its(:content) { should match(/uwsgi/) }
   end
 end
 
-
 describe 'configures newrelic meetme for redis' do
   context file('/etc/newrelic/newrelic-plugin-agent.cfg') do
-    its(:content) { should match(/redis/)}
+    its(:content) { should match(/redis/) }
   end
 end
 
 describe 'configures newrelic meetme for rabbitmq' do
   context file('/etc/newrelic/newrelic-plugin-agent.cfg') do
-    its(:content) { should match(/rabbitmq/)}
-    its(:content) { should match(/15672/)}
+    its(:content) { should match(/rabbitmq/) }
+    its(:content) { should match(/15672/) }
   end
 
 end
