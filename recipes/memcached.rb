2cc5c8645feebc3985f4bd8d764f84be58a0ea84
cybermerc <nomad@cybermerc.org>
2015-01-08 15:47:20 -0600

    added search to iptables setup in memcached

--- recipes/memcached.rb
+++ recipes/memcached.rb
@@ -17,12 +17,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 include_recipe 'chef-sugar'
 include_recipe 'apt' if platform_family?('debian')
 include_recipe 'memcached'
 
 # iptables
-add_iptables_rule('INPUT', "-m tcp -p tcp --dport #{node['memcached']['port']} -j ACCEPT", 9999, 'Open TCP port for memcache')
-add_iptables_rule('INPUT', "-m udp -p udp --dport #{node['memcached']['port']} -j ACCEPT", 9999, 'Open UDP port for memcache')
+search_add_iptables_rules("tags:#{stackname.gsub('stack', '')}_app_node AND chef_environment:#{node.chef_environment}",
+                          'INPUT',
+                          "-m tcp -p tcp --dport #{node['memcached']['port']} -j ACCEPT",
+                          9999,
+                          'Open port for memcached from app')
+
+search_add_iptables_rules("tags:#{stackname.gsub('stack', '')}_app_node AND chef_environment:#{node.chef_environment}",
+                          'INPUT',
+                          "-m tcp -p udp --dport #{node['memcached']['port']} -j ACCEPT",
+                          9999,
+                          'Open port for memcached from app')
+
 
 node.set['platformstack']['cloud_monitoring']['plugins']['memcached']['disabled'] = false
